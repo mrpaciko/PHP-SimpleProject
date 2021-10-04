365f184b070cd9c77165f32cb2cf8a1a5ce13536
Bang Beng <55169996+banggiovanni@users.noreply.github.com>
2021-10-04 15:11:41 +0700

    random-username.php

--- bang.php
+++ bang.php
@@ -1,46 +1,26 @@
 <?php
-
-function getStr($string,$start,$end){
-$str = explode($start,$string);
-$str = explode($end,$str[1]);
-return $str[0];
-}
-$url = "https://www.blibli.com/";
+//FAKE DATA
 $ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://randomuser.me/api/');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
 $headers = array();
-$headers[] = 'Authority: www.blibli.com';
+$headers[] = 'Authority: randomuser.me';
 $headers[] = 'Cache-Control: max-age=0';
-$headers[] = 'Sec-Ch-Ua: \" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"';
-$headers[] = 'Sec-Ch-Ua-Mobile: ?0';
 $headers[] = 'Upgrade-Insecure-Requests: 1';
-$headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 11.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
-$headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
+$headers[] = 'User-Agent: Mozilla/5.0 (Linux; U; Android 4.1; en-us; GT-N7100 Build/JRO03C) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30';
+$headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
 $headers[] = 'Sec-Fetch-Site: none';
 $headers[] = 'Sec-Fetch-Mode: navigate';
 $headers[] = 'Sec-Fetch-User: ?1';
 $headers[] = 'Sec-Fetch-Dest: document';
-$headers[] = 'Accept-Language: id-ID,id;q=0.9';
+$headers[] = 'Accept-Language: en-US,en;q=0.9,id;q=0.8';
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-$headers = [];
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-// this function is called by curl for each header received
-curl_setopt($ch, CURLOPT_HEADERFUNCTION,
-  function($curl, $header) use (&$headers)
-  {
-    $len = strlen($header);
-    $header = explode(':', $header, 2);
-    if (count($header) < 2) // ignore invalid headers
-      return $len;
-
-    $headers[strtolower(trim($header[0]))][] = trim($header[1]);
-
-    return $len;
-  }
-);
-
-$data = curl_exec($ch);
-print_r($headers);
+$result = curl_exec($ch);
+$d = json_decode($result,true);
+$username = $d['results']['0']['login']['username'];
+echo $username;
 ?>
