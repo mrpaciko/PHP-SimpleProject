b370796a0d66d2df33cbfb878dd1083e4a44348f
ripyourlife <72772297+ripyourlife@users.noreply.github.com>
2021-10-19 18:03:14 +0700

    Update Sha1.php

--- Sha1.php
+++ Sha1.php
@@ -1,15 +1,6 @@
 <?php
-set_time_limit(0);
-error_reporting(0);
-function generateRandomString($length = 10) {
-$characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-$charactersLength = strlen($characters);
-$randomString = '';
-for ($i = 0; $i < $length; $i++) {
-$randomString .= $characters[rand(0, $charactersLength - 1)];
-}
-return $randomString;
-}
-$dua = generateRandomString(2);
-echo $dua;
+//RANDOM NUMBER
+echo(rand() . "<br>");
+echo(rand() . "<br>");
+echo(rand(999,9999));
 ?>
